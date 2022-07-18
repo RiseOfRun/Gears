c6da2b63f26855cb2d9e9c085e83088bc447fe3a
Runi <runi54rus@gmail.com>
2022-07-18 09:35:48 +0700

    Custom Editor

--- Assets/Scripts/Axle.cs
+++ Assets/Scripts/Axle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 public class Axle : MonoBehaviour
-{ 
+{
     [FormerlySerializedAs("DefaultMovingGearPrefab")] public Gear DefaultGearPrefab;
     [FormerlySerializedAs("TargetMovingGear")] [SerializeField] public Gear TargetGear;
     [HideInInspector] public Gear CurrentGear;
--- Assets/Scripts/Gear.cs
+++ Assets/Scripts/Gear.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
-
+[SelectionBase]
 public class Gear : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public int GearType;
